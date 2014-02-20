44e7fe86df4b5f6acf801f78f20f07d0d6a7464e
Allen Rohner <arohner@gmail.com>
2014-02-20 12:10:40 -0600

    Adds voom

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.2"]
-                 [cheshire "5.1.1"]])
-
+                 [cheshire "5.1.1"]]
+  :plugins [[lein-voom "0.1.0-20140122_084541-g72df37a"]])
